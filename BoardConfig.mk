94264025b6ffd22a4e389a8854a270f718355e25
Larry Schiefer <ljschiefer@gmail.com>
2020-05-19 11:24:15 -0500

    Set HDMI output to be 1080p. Set Acme One device to be hikey960 so the various bits are built and packaged correctly.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,2 +1,4 @@
 include device/linaro/hikey/hikey960/BoardConfig.mk
 
+BOARD_KERNEL_CMDLINE += video=HDMI-A-1:1920x1080@60
+
--- acme_one.mk
+++ acme_one.mk
@@ -1,3 +1,7 @@
+# Pick up HiKey960 proprietary bits FIRST
+$(call inherit-product-if-exists, vendor/linaro/hikey960/hisilicon/device-partial.mk)
+
+
 # Inherit from the HiKey960 based definition
 $(call inherit-product, device/linaro/hikey/hikey960.mk)
 
@@ -5,12 +9,6 @@ $(call inherit-product, device/linaro/hikey/hikey960.mk)
 # Overrides
 PRODUCT_MANUFACTURER := Acme
 PRODUCT_NAME := acme_one
-PRODUCT_DEVICE := acme_one
+PRODUCT_DEVICE := hikey960
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on acme_one
-
-
-# Pick up HiKey960 proprietary bits
-$(call inherit-product-if-exists, vendor/linaro/hikey960/hisilicon/device-partial.mk)
-
-
